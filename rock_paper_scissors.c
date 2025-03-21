a9d8e24e582e30ca2c7031fdc43e366b71f7692e
OutOfNet <neteoris@proton.me>
2025-03-21 06:52:30 +0000

    added the source code

---
+++ rock_paper_scissors.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	rand();
+	rand();
+	rand();
+	srand(time(NULL));
+	float computer_move = rand() % 3;
+	printf("%d", RAND_MAX);
+	printf("\n%d", computer_move);
+
+	return 0;
+}
